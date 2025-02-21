b35aa2e87f0f36592ea0abdab059cd921ce62610
Miruto80 <miguelfernando8012@gmail.com>
2025-02-21 18:16:17 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Preyecto III</title>
+</head>
+<body>
+    <h1>Prueba de miguel</h1>
+    <!--Coloquen un h1 para probar y avisan para hacer git pull-->
+</body>
+</html>
