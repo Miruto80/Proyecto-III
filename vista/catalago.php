6bcb7df04e0564497bf2db150b723fbeaf5ff03b
Erick09s <erickdb9@gmail.com>
2025-04-29 22:56:01 -0400

    prueba

--- vista/catalago.php
+++ vista/catalago.php
@@ -21,6 +21,7 @@
 		<section class="textos-header">
 			<h1>LOVEMAKEUP</h1>
 			<h2>Venta al mayor y detal de productos de maquillaje</h2>
+			<h2>erick</h2>
 		</section>
 		<div class="ola" style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #fff;"></path></svg></div>
 	</header>
