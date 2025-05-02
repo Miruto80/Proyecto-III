d94c5387076852090439707e3980c7e1b153e2b9
Rhichardv <virguezrhichard11@gmail.com>
2025-05-01 23:31:32 -0400

    u

--- vista/categoria.php
+++ vista/categoria.php
@@ -2,7 +2,7 @@
 <html lang="es">
 <head>
   <?php include 'complementos/head.php'; ?> 
-  <title> Categoria | LoveMakeup  </title> 
+  <title> Categoriaa | LoveMakeup  </title> 
 </head>
 <body class="g-sidenav-show bg-gray-100">
   <?php include 'complementos/sidebar.php'; ?>
