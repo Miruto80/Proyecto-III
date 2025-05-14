b36eda57806a5a14284b44350febd6621fbf2875
Miruto80 <miguelfernando8012@gmail.com>
2025-05-14 17:03:30 -0400

    Boton de añadir a favoritos

--- vista/tienda/catalogo.php
+++ vista/tienda/catalogo.php
@@ -145,54 +145,56 @@
 
                 <!-- GRIP DE PRODUCTO -->
                 <div class="row">
-                    <div class="col-md-12">
-                        <div class="product-grid row row-cols-2 row-cols-sm-2 row-cols-md-3 row-cols-lg-3 row-cols-xl-4 g-4">
-                            <?php foreach ($registro as $producto): ?>
-                                <div class="col">
-                                    <div class="product-item" data-categoria="<?php echo $producto['id_categoria']; ?>"data-bs-toggle="modal"
-                                     data-bs-target="#productModal"
-	                                   data-id="<?php echo $producto['id_producto']; ?>"
-                                     data-nombre="<?php echo htmlspecialchars($producto['nombre']); ?>"
-                                     data-precio="<?php echo $producto['precio_detal']; ?>"
-                                     data-marca="<?php echo htmlspecialchars($producto['marca']); ?>"
-                                     data-descripcion="<?php echo htmlspecialchars($producto['descripcion']); ?>"
-                                     data-cantidad-mayor="<?php echo $producto['cantidad_mayor']; ?>"
-                                     data-precio-mayor="<?php echo $producto['precio_mayor']; ?>"
-                                     data-stock-disponible="<?php echo $producto['stock_disponible']; ?>"
-                                     data-imagen="<?php echo $producto['imagen']; ?>"
-	                                   data-categoria="<?php echo $producto['id_categoria']; ?>"
-                                     onclick="openModal(this)">
+    <div class="col-md-12">
+        <div class="product-grid row row-cols-2 row-cols-sm-2 row-cols-md-3 row-cols-lg-3 row-cols-xl-4 g-4">
+            <?php foreach ($registro as $producto): ?>
+                <div class="col">
+                    <div class="product-item" data-categoria="<?php echo $producto['id_categoria']; ?>" data-bs-toggle="modal"
+                         data-bs-target="#productModal"
+                         data-id="<?php echo $producto['id_producto']; ?>"
+                         data-nombre="<?php echo htmlspecialchars($producto['nombre']); ?>"
+                         data-precio="<?php echo $producto['precio_detal']; ?>"
+                         data-marca="<?php echo htmlspecialchars($producto['marca']); ?>"
+                         data-descripcion="<?php echo htmlspecialchars($producto['descripcion']); ?>"
+                         data-cantidad-mayor="<?php echo $producto['cantidad_mayor']; ?>"
+                         data-precio-mayor="<?php echo $producto['precio_mayor']; ?>"
+                         data-stock-disponible="<?php echo $producto['stock_disponible']; ?>"
+                         data-imagen="<?php echo $producto['imagen']; ?>"
+                         onclick="openModal(this)">
 
-                                        <figure>
-                                            <p title="<?php echo htmlspecialchars($producto['nombre']); ?>">
-                                                <img src="<?php echo $producto['imagen']; ?>" alt="<?php echo htmlspecialchars($producto['nombre']); ?>" class="tab-image img-fluid rounded-3">
-                                            </p>
-                                        </figure>
-                                        <div class="d-flex flex-column text-center">
-                                            <h3 class="fs-5 fw-normal">
-                                                <p href="#" class="text-decoration-none">
-                                                    <?php echo htmlspecialchars($producto['nombre']); ?>
-                                                </p>
-                                            </h3>
-                                            <div class="d-flex justify-content-center align-items-center gap-2">
-                                                <?php if ($producto['precio_mayor']): ?>
-                                                    <span class="text-dark fw-semibold"> M $<?php echo $producto['precio_mayor']; ?></span>
-                                                <?php endif; ?>
-                                                <span class="text-dark fw-semibold">D $<?php echo $producto['precio_detal']; ?></span>
-                                            </div>
-                                            <div class="button-area p-3">
-                                                <button class="btn btn-dark rounded-1 p-2 fs-7 btn-cart">
-                                       <i class="fa-solid fa-cart-shopping"></i> Añadir al carrito
-                                    </button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            <?php endforeach; ?>
+                        <figure class="position-relative">
+                            <p title="<?php echo htmlspecialchars($producto['nombre']); ?>">
+                                <img src="<?php echo $producto['imagen']; ?>" alt="<?php echo htmlspecialchars($producto['nombre']); ?>" class="tab-image img-fluid rounded-3">
+                            </p>
+                            <button class="btn btn-light position-absolute top-0 end-0 m-2" title="Añadir a favoritos">
+                                <i class="fa-solid fa-heart"></i>
+                            </button>
+                        </figure>
+                        <div class="d-flex flex-column text-center">
+                            <h3 class="fs-5 fw-normal">
+                                <p href="#" class="text-decoration-none">
+                                    <?php echo htmlspecialchars($producto['nombre']); ?>
+                                </p>
+                            </h3>
+                            <div class="d-flex justify-content-center align-items-center gap-2">
+                                <?php if ($producto['precio_mayor']): ?>
+                                    <span class="text-dark fw-semibold"> M $<?php echo $producto['precio_mayor']; ?></span>
+                                <?php endif; ?>
+                                <span class="text-dark fw-semibold">D $<?php echo $producto['precio_detal']; ?></span>
+                            </div>
+                            <div class="button-area p-3">
+                                <button class="btn btn-dark rounded-1 p-2 fs-7 btn-cart">
+                                    <i class="fa-solid fa-cart-shopping"></i> Añadir al carrito
+                                </button>
+                            </div>
                         </div>
-                        
                     </div>
                 </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+</div>
+
 
                 <div class="text-center mt-4">
                     <a href="?pagina=catalogo_producto" class="btn btn-primary btn-lg px-5">Ver Todos los productos</a>
