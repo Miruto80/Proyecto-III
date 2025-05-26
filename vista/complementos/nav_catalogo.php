c9bcac19c7793a3a310ac310402778c32bdcd840
Miruto80 <miguelfernando8012@gmail.com>
2025-05-26 18:30:03 -0400

    Tasa con api

--- vista/complementos/nav_catalogo.php
+++ vista/complementos/nav_catalogo.php
@@ -43,15 +43,26 @@ nav {
 
 <script>
               //obtener tasa del localstorage
-             document.addEventListener("DOMContentLoaded", function () {
-              setInterval(function () {
-                let valor = localStorage.getItem("tasadeldia");
-                 if (valor) {
-                document.getElementById("bcv").textContent = "Tasa del Dia: " + valor +"Bs";
-               }
-              }, 1);
-    
-              });
+              async function obtenerTasaDolarApi() {
+    try {
+        const respuesta = await fetch('https://ve.dolarapi.com/v1/dolares/oficial');
+        if (!respuesta.ok) {
+            throw new Error(`Error HTTP: ${respuesta.status}`);
+        }
+
+        const datos = await respuesta.json();
+        const tasaBCV = datos.promedio.toFixed(2); // Redondea la tasa a 2 decimales
+
+        document.getElementById("bcv").textContent = "Tasa del Día: " + tasaBCV + " Bs";
+    } catch (error) {
+        document.getElementById("bcv").textContent = "Error al cargar la tasa";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", obtenerTasaDolarApi);
+
+
+
 
 </script>
 
@@ -220,6 +231,5 @@ window.addEventListener("scroll", () => {
 
 
   </script>
-  <script src="assets/js/tasa.js"></script>
 
 
