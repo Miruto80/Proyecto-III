380d2270eb59e5e2201056763877061a89ae5c88
danielsan7875 <danielsanchezcev@gmail.com>
2025-05-28 12:22:05 -0400

    .

--- modelo/tipousuario.php
+++ modelo/tipousuario.php
@@ -1,22 +1,26 @@
 <?php
 require_once 'conexion.php';
 class tipousuario extends Conexion {
-    private $conex;
+    private $conex1;
+    private $conex2;
     private $id_tipo;
     private $nombre;
     private $nivel;
     private $estatus;
 
     function __construct() {
-        $this->conex = new Conexion();
-        $this->conex = $this->conex->Conex();
+        parent::__construct(); // Llama al constructor de la clase padre
+
+        // Obtener las conexiones de la clase padre
+        $this->conex1 = $this->getConex1();
+        $this->conex2 = $this->getConex2();
     }
 
     public function registrarBitacora($id_persona, $accion, $descripcion) {
     $consulta = "INSERT INTO bitacora (accion, fecha_hora, descripcion, id_persona) 
                  VALUES (:accion, NOW(), :descripcion, :id_persona)";
     
-    $strExec = $this->conex->prepare($consulta);
+    $strExec = $this->conex2->prepare($consulta);
     $strExec->bindParam(':accion', $accion);
     $strExec->bindParam(':descripcion', $descripcion);
     $strExec->bindParam(':id_persona', $id_persona);
@@ -26,7 +30,7 @@ class tipousuario extends Conexion {
 
     public function registrar() {
         $registro = "INSERT INTO rol_usuario(nombre, nivel, estatus) VALUES (:nombre, :nivel, :estatus)";
-        $strExec = $this->conex->prepare($registro);
+        $strExec = $this->conex2->prepare($registro);
         $strExec->bindParam(':nombre', $this->nombre);
         $strExec->bindParam(':nivel', $this->nivel);
         $strExec->bindParam(':estatus', $this->estatus);
@@ -36,7 +40,7 @@ class tipousuario extends Conexion {
 
     public function modificar() {
         $registro = "UPDATE rol_usuario SET nombre = :nombre, nivel = :nivel, estatus = :estatus WHERE id_tipo = :id_tipo";
-        $strExec = $this->conex->prepare($registro);
+        $strExec = $this->conex2->prepare($registro);
         $strExec->bindParam(':nombre', $this->nombre);
         $strExec->bindParam(':nivel', $this->nivel);
         $strExec->bindParam(':estatus', $this->estatus);
@@ -47,7 +51,7 @@ class tipousuario extends Conexion {
 
     public function eliminar() {
         $registro = "DELETE FROM rol_usuario WHERE id_tipo = :id_tipo";
-        $strExec = $this->conex->prepare($registro);
+        $strExec = $this->conex2->prepare($registro);
         $strExec->bindParam(':id_tipo', $this->id_tipo);
         $resul = $strExec->execute();
         return $resul ? ['respuesta' => 1, 'accion' => 'eliminar'] : ['respuesta' => 0, 'accion' => 'eliminar'];
@@ -55,7 +59,7 @@ class tipousuario extends Conexion {
 
     public function consultar() {
         $registro = "SELECT * FROM rol_usuario";
-        $consulta = $this->conex->prepare($registro);
+        $consulta = $this->conex2->prepare($registro);
         $resul = $consulta->execute();
         return $resul ? $consulta->fetchAll(PDO::FETCH_ASSOC) : [];
     }
