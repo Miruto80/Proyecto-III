e70122a400e5eeb0e72b8fc4966ceff3a0ee6fae
Erick09s <erickdb9@gmail.com>
2025-05-29 08:42:46 -0400

    visita guiada pedidoweb

--- assets/js/pedidoweb.js
+++ assets/js/pedidoweb.js
@@ -82,3 +82,28 @@ $(document).on('click', '.btn-eliminar', function () {
       }
   });
 });
+
+$('#btnAyuda').on("click", function () {
+  
+    const driver = window.driver.js.driver;
+    
+    const driverObj = new driver({
+      nextBtnText: 'Siguiente',
+          prevBtnText: 'Anterior',
+          doneBtnText: 'Listo',
+      popoverClass: 'driverjs-theme',
+      closeBtn:false,
+      steps: [
+        { element: '.table-color', popover: { title: 'Tabla de Pedidos', description: 'Aqui es donde se guardaran los registros de los pedidos web', side: "left", }},
+        { element: '.ver-detalles', popover: { title: 'Boton de ver detalle', description: 'Darle click aqui te llevara a un modal para poder ver los detalles del pedido', side: "bottom", align: 'start' }},
+        { element: '.btn-validar', popover: { title: 'validar pedido', description: 'Este botón te permite validar un pedido cuando el pago es confirmado', side: "left", align: 'start' }},
+        { element: '.btn-eliminar', popover: { title: 'Eliminar pedido', description: 'Usa este botón para eliminar un pedido', side: "left", align: 'start' }},
+        { element: '.dt-search', popover: { title: 'Buscar', description: 'Te permite buscar un pedido especifico ', side: "right", align: 'start' }},
+        { popover: { title: 'Eso es todo', description: 'Este es el fin de la guia espero hayas entendido'} }
+      ]
+    });
+    
+    // Iniciar el tour
+    driverObj.drive();
+  });
+  
--- vista/pedidoweb.php
+++ vista/pedidoweb.php
@@ -8,6 +8,76 @@
   <title> Pedido Web | LoveMakeup  </title> 
 </head>
 
+
+<style>
+  .driver-popover.driverjs-theme {
+  color: #000;
+}
+
+.driver-popover.driverjs-theme .driver-popover-title {
+  font-size: 20px;
+}
+
+.driver-popover.driverjs-theme .driver-popover-title,
+.driver-popover.driverjs-theme .driver-popover-description,
+.driver-popover.driverjs-theme .driver-popover-progress-text {
+  color: #000;
+}
+
+.driver-popover.driverjs-theme button {
+  flex: 1;
+  text-align: center;
+  background-color: #000;
+  color: #ffffff;
+  border: 2px solid #000;
+  text-shadow: none;
+  font-size: 14px;
+  padding: 5px 8px;
+  border-radius: 6px;
+}
+
+.driver-popover.driverjs-theme button:hover {
+  background-color: #000;
+  color: #ffffff;
+}
+
+.driver-popover.driverjs-theme .driver-popover-navigation-btns {
+  justify-content: space-between;
+  gap: 3px;
+}
+
+.driver-popover.driverjs-theme .driver-popover-close-btn {
+  color: #fff;
+  width: 20px; /* Reducir el tamaño del botón */
+  height: 20px;
+  font-size: 16px;
+  transition: all 0.5 ease-in-out;
+}
+
+.driver-popover.driverjs-theme .driver-popover-close-btn:hover {
+ background-color: #fff;
+ color: #000;
+ border: #000;
+}
+
+.driver-popover.driverjs-theme .driver-popover-arrow-side-left.driver-popover-arrow {
+  border-left-color: #fde047;
+}
+
+.driver-popover.driverjs-theme .driver-popover-arrow-side-right.driver-popover-arrow {
+  border-right-color: #fde047;
+}
+
+.driver-popover.driverjs-theme .driver-popover-arrow-side-top.driver-popover-arrow {
+  border-top-color: #fde047;
+}
+
+.driver-popover.driverjs-theme .driver-popover-arrow-side-bottom.driver-popover-arrow {
+  border-bottom-color: #fde047;
+}
+
+</style>
+
 <body class="g-sidenav-show bg-gray-100">
 
 
@@ -42,14 +112,20 @@
        <h4 class="mb-0"><i class="fa-solid fa-desktop mr-2" style="color: #f6c5b4;"></i>
         Pedido Web</h5>
       </div>
+            <button style="margin-left: 90%;" type="button" class="btn btn-primary" id="btnAyuda">
+    <span class="icon text-white">
+      <i class="fas fa-info-circle"></i>
+    </span>
+    <span class="text-white">Ayuda</span>
+  </button>
           
       </div>
-          
+    
 
       <div class="table-responsive m-3"> <!-- comienzo div table-->
            <!-- comienzo de tabla-->                      
           <table class="table table-bordered table-hover" id="myTable" width="100%" cellspacing="0">
-              <thead class="table-color">
+              <thead  class="table-color">
                 <tr>
                   <th class="text-white" style="display: none;">ID</th>
                   <th class="text-white" style="display: none;">Tipo</th>
