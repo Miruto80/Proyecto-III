291c59a7702b10c3d0cb13b4d03324e2da7cfc4a
danielsan7875 <danielsanchezcev@gmail.com>
2025-07-07 14:47:54 -0400

    PERMISOS WEB

--- vista/pedidoweb.php
+++ vista/pedidoweb.php
@@ -204,7 +204,7 @@
     data-bs-target="#verDetallesModal<?= $pedido['id_pedido']; ?>">
  <i class="fa fa-eye"></i> 
 </button>
-
+<?php if ($_SESSION["nivel_rol"] >= 2 && tieneAcceso(9, 'especial')): ?>
 <button type="button" class="btn  btn-primary" data-bs-toggle="modal" data-bs-target="#modalTracking<?php echo $pedido['id_pedido']; ?>"><i class="fa-regular fa-envelope"></i>
 
 </button>
@@ -222,7 +222,7 @@
 
   <?php endif; ?>
 
-
+ <?php endif; ?>
       
     </td>
 </tr>
