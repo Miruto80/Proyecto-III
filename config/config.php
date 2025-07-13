b60d832b6501e10c9e3d5fec5e8b5b32f300e125
Miruto80 <miguelfernando8012@gmail.com>
2025-07-13 10:31:50 -0400

    Poniendo bien la bd jasdjsadj

--- config/config.php
+++ config/config.php
@@ -1,9 +1,10 @@
 <?php
  
-define('_DB_NAME_1_', 'lovemakeupbd');
-define('_DB_NAME_2_', 'lovemakeupbds');
+define('_DB_NAME_1_', 'u417714339_Lovemakeupbd');
+define('_DB_NAME_2_', 'u417714339_lovemakeupbds');
 define('_DB_HOST_', 'localhost');
-define('_DB_USER_', 'root');
-define('_DB_PASS_', ''); 
+define('_DB_USER_', 'u417714339_Lovemakeup');
+define('_DB_USER_2_', 'u417714339_lovemakeups');
+define('_DB_PASS_', 'Labiosfresa123.'); 
 
 date_default_timezone_set('America/Caracas');
