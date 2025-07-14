f3b5643c53bcd9171186c48846320e2bca75f9dd
Erick09s <erickdb9@gmail.com>
2025-07-14 00:38:02 -0400

    .

--- vista/pedidoweb.php
+++ vista/pedidoweb.php
@@ -217,12 +217,14 @@
   </button>
 <?php endif; ?> -->
 
-<button type="button" class="btn btn-secundary .btn-eliminar btn-success"  data-id="<?= $pedido['id_pedido'] ?>">
+
 
 <?php if (!in_array($pedido['estado'], [0,2,3,4,5])): ?>
 <button type="button" class="btn btn-secundary btn-validar btn-success"  data-id="<?= $pedido['id_pedido'] ?>">
 <i class="fa-solid fa-check"></i>
   </button>
+
+  <button type="button" class="btn btn-secundary .btn-eliminar btn-success"  data-id="<?= $pedido['id_pedido'] ?>">
 
   <?php endif; ?>
 
